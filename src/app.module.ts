d2d7d7449a3577b0c5137c9e7c125273a7d2344b
qzhao19 <qi.zhao@outlook.fr>
2025-09-14 20:19:12 +0800

    create app.module.ts file

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ModelModule } from './llm/model/model.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env', '.env.local'],
+      cache: true,
+      expandVariables: true,
+    }),
+    ModelModule,
+  ],
+  controllers: [], 
+  providers: [],
+})
+export class AppModule {}
