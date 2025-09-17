8fe07fc45289e5af5f3fbe5b33945255fa8b163f
qzhao19 <qi.zhao@outlook.fr>
2025-09-17 23:41:14 +0800

    create llm.types.ts

---
+++ src/types/llm.types.ts
@@ -0,0 +1,25 @@
+export interface MacroAnalysis {
+  marketSize: string;
+  growthRates: string;
+  forecasts: string;
+  marketStage: string;
+  macroEconomie: string,
+  policies: string
+};
+
+export interface SegmentationAnalysis {
+  productSegments: string;
+  userSegments: any;
+  geographicSegments: any;
+};
+
+export interface TrendAnalysis {
+  technology: any;
+  policy: any;
+  socioeconomic: any;
+  supplyChain: any;
+  competitor: string
+};
+
+
+
