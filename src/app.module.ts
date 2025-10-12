07dee2260da52bfa2bacce54d636bb87fb4d323f
qzhao19 <qi.zhao@outlook.fr>
2025-10-12 22:06:15 +0800

    update code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,10 @@
 import { Module } from "@nestjs/common";
-import { ConfigModule } from "@nestjs/config";
-import { ModelModule } from "./llm/model/model.module";
+import { AppConfigModule } from "./config/config.module"; 
 import { AgentModule } from "./llm/agents/agent.module";
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: [".env", ".env.local"],
-      cache: true,
-      expandVariables: true,
-    }),
-    ModelModule,
+    AppConfigModule,
     AgentModule,
   ],
   controllers: [], 
