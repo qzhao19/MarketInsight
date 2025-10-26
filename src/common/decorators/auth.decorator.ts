4f29a2ce6a4fef2c2d49f618c97e23f35c163606
qzhao19 <qi.zhao@outlook.fr>
2025-10-26 23:30:40 +0800

    create auth.decorator file

---
+++ src/common/decorators/auth.decorator.ts
@@ -0,0 +1,22 @@
+import { SetMetadata } from '@nestjs/common';
+import { CustomDecorator } from '@nestjs/common';
+
+export const IS_PUBLIC_KEY = "isPublic";
+
+/**
+ * Allows unauthenticated (anonymous) access to a route or controller.
+ * When applied, the AuthGuard will not perform authentication checks.
+ * 
+ * @example
+ * ```typescript
+ * @Get('public')
+ * @Public()
+ * getPublicData() {
+ *   return { message: 'This is public' };
+ * }
+ * ```
+ */
+export const Public = (): CustomDecorator<string> => 
+  SetMetadata(IS_PUBLIC_KEY, true);
+
+
