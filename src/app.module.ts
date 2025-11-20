5eb810835df5e257ceacd4a260125ed31fd2a152
qzhao19 <qi.zhao@outlook.fr>
2025-11-20 23:34:55 +0800

    update code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from "@nestjs/common";
 import { AppConfigModule } from "./config/config.module"; 
-import { AgentModule } from "./llm/agents/agent.module";
+import { AgentModule } from "./agent/agent.module";
+import { ApiModule } from "./api/api.module";
+import { LLModelModule } from "./llm/model.module";
 
 @Module({
   imports: [
     AppConfigModule,
     AgentModule,
+    ApiModule,
+    LLModelModule,
   ],
   controllers: [], 
   providers: [],
