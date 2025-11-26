ea41830f837cf6e5f0b4d50f14eb3a7435ecdb7a
qzhao19 <qi.zhao@outlook.fr>
2025-11-26 23:24:01 +0800

    add model.module

---
+++ src/core/llm/model.module.ts
@@ -0,0 +1,23 @@
+import { Module, forwardRef } from "@nestjs/common";
+import { LLModelService } from "./model.service";
+import { LLModelClientModule } from "./client/client.module";
+import { AppConfigModule } from "../../config/config.module";
+
+/**
+ * Module for managing LLM models
+ * 
+ * This module:
+ * 1. Imports AppConfigModule to access configuration services
+ * 2. Provides LLModelService for creating and managing LLM models
+ * 3. Imports LLModelClientModule for client protection mechanisms
+ */
+@Module({
+  imports: [
+    AppConfigModule,
+    forwardRef(() => LLModelClientModule)
+  ],
+  controllers: [],
+  providers: [LLModelService],
+  exports: [LLModelService],
+})
+export class LLModelModule {}
