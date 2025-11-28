d84e063247f06af516c58fe295c3df09abf0130c
qzhao19 <qi.zhao@outlook.fr>
2025-11-29 00:02:18 +0800

    update code

--- src/core/database/prisma.module.ts
+++ src/core/database/prisma.module.ts
@@ -1,5 +1,5 @@
-import { Module } from '@nestjs/common';
-import { PrismaService } from './prisma.service';
+import { Module } from "@nestjs/common";
+import { PrismaService } from "./prisma.service";
 
 /**
  * A module that provides the PrismaService for database operations.
