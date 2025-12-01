41024589cbcfc1aedeea20c93208cf588b215544
qzhao19 <qi.zhao@outlook.fr>
2025-12-01 21:09:53 +0800

    create index.dto

---
+++ src/modules/user/dto/index.ts
@@ -0,0 +1,11 @@
+// ==================== Request DTOs ====================
+export * from "./request/login.request.dto";
+export * from "./request/register.request.dto";
+export * from "./request/update-user.request.dto";
+export * from "./request/list-users.request.dto";
+
+// ==================== Response DTOs ====================
+export * from "./response/user.response.dto";
+export * from "./response/login.response.dto";
+export * from "./response/user-stats.response.dto";
+export * from "./response/paginated-users.response.dto";
