3486a9816fb57d8aaa7a24b91aab6a825321eb00
qzhao19 <qi.zhao@outlook.fr>
2025-12-01 21:07:54 +0800

    create update-user.request.dto

---
+++ src/modules/user/dto/request/update-user.request.dto.ts
@@ -0,0 +1,40 @@
+import { 
+  IsString, 
+  IsOptional,
+  IsNotEmpty, 
+  MinLength, 
+  MaxLength, 
+  Matches,
+} from "class-validator";
+
+/**
+ * Data Transfer Object for updating user information
+ * All fields are optional - only provided fields will be updated
+ */
+export class UpdateUserRequestDto {
+  @IsOptional()
+  @IsString({ message: "Username must be a string" })
+  @IsNotEmpty({ message: "Username cannot be empty" })
+  @MinLength(3, { message: "Username must be at least 3 characters" })
+  @MaxLength(20, { message: "Username cannot exceed 20 characters" })
+  @Matches(
+    /^[a-zA-Z][a-zA-Z0-9_]*$/, 
+    { 
+      message: "Username must start with a letter and can only contain letters, numbers, and underscores" 
+    }
+  )
+  username?: string;
+
+  @IsOptional()
+  @IsString({ message: "Password must be a string" })
+  @IsNotEmpty({ message: "Password cannot be empty" })
+  @MinLength(8, { message: "Password must be at least 8 characters" })
+  @MaxLength(100, { message: "Password cannot exceed 100 characters" })
+  @Matches(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#^()_+=\-[\]{}|;:'",.<>/\\`~])[A-Za-z\d@$!%*?&#^()_+=\-[\]{}|;:'",.<>/\\`~]{8,}$/,
+    {
+      message: "Password must contain at least 1 uppercase letter, 1 lowercase letter, 1 digit, and 1 special character",
+    }
+  )
+  password?: string;
+}
