59b43ee9c117f98a520e45c5de37a70ee5f5867d
qzhao19 <qi.zhao@outlook.fr>
2025-12-10 23:23:08 +0800

    add index.ts

---
+++ src/modules/campaign/dto/index.ts
@@ -0,0 +1,7 @@
+// ==================== Request DTOs ====================
+export * from "./request/agent-options.request.dto";
+export * from "./request/create-campaign.request.dto";
+export * from "./request/query-campaign.request.dto";
+
+// ==================== Response DTOs ====================
+export * from "./response/campaign.response.dto";
