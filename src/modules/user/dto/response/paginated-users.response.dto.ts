18b3c2504842627c87b45205f459ba4f899a7378
qzhao19 <qi.zhao@outlook.fr>
2025-12-13 22:34:51 +0800

    update code

--- src/modules/user/dto/response/paginated-users.response.dto.ts
+++ src/modules/user/dto/response/paginated-users.response.dto.ts
@@ -1,4 +1,5 @@
 import { Expose } from "class-transformer";
+import { ApiProperty } from "@nestjs/swagger";
 import { UserResponseDto } from "./user.response.dto";
 import { SafeUser } from "../../../../common/types/database/entity.types";
 
@@ -6,21 +7,27 @@ import { SafeUser } from "../../../../common/types/database/entity.types";
  * Pagination metadata
  */
 export class PaginationMeta {
+  @ApiProperty({ description: "Total number of users", example: 100 })
   @Expose()
   total: number;
 
+  @ApiProperty({ description: "Number of users skipped", example: 0 })
   @Expose()
   skip: number;
 
+  @ApiProperty({ description: "Number of users returned", example: 20 })
   @Expose()
   take: number;
 
+  @ApiProperty({ description: "Whether more users are available", example: true })
   @Expose()
   hasMore: boolean;
 
+  @ApiProperty({ description: "Total number of pages", example: 5 })
   @Expose()
   totalPages: number;
 
+  @ApiProperty({ description: "Current page number", example: 1 })
   @Expose()
   currentPage: number;
 }
@@ -29,9 +36,17 @@ export class PaginationMeta {
  * Paginated users response DTO
  */
 export class PaginatedUsersResponseDto {
+  @ApiProperty({
+    description: "Array of users",
+    type: [UserResponseDto],
+  })
   @Expose()
   data: UserResponseDto[];
 
+  @ApiProperty({
+    description: "Pagination metadata",
+    type: PaginationMeta,
+  })
   @Expose()
   pagination: PaginationMeta;
 
